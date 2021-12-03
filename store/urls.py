568ee9257dc8afebcabd0765e80357f9946d4bfe
Jonathan Nzete <jonathannzete@gmail.com>
2021-12-03 14:16:49 +0100

    edit and delete functionality

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
     path('add_review/', views.AddReview.as_view(), name='add_review'),
+    path('update_review/<int:pk>/', views.UpdateReview.as_view(), name='update_review'),
+    path('delete_review/<int:pk>/', views.DeleteReview.as_view(), name='delete_review'),
     path('<str:slug>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,16 +1,16 @@
-from django.http import request
 from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import permissions
 
-
+from orders.models import OrderProduct
 from .serializers import ProductSerializer, ReviewSerialzer
-from .models import Product
+from .models import Product, Review
 
 
 class ProductList(APIView):
-    
+
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
@@ -18,18 +18,66 @@ class ProductList(APIView):
 
 
 class ProductDetail(APIView):
-    
+
     def get(self, request, slug):
+        user = request.user
+        is_purchased = False
+        already_reviewed = False
         product = Product.objects.get(slug=slug)
+        if user.is_authenticated:
+            is_purchased = OrderProduct.objects.filter(
+                product__slug=slug, user=user).exists()
+            # check if user already submitted a review for this product
+            already_reviewed = Review.objects.filter(product=product, user=user).exists() 
         serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        return Response({'product': serializer.data, 'is_purchased': is_purchased, 'already_reviewed': already_reviewed})
 
 
 class AddReview(APIView):
-    
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request):
         serializer = ReviewSerialzer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-        
+            data = serializer.data
+            current_user = request.user
+            product = Product.objects.get(pk=data['product'])
+
+            review = Review()
+            review.user = current_user
+            review.product = product
+            review.subject = data['subject']
+            review.review = data['review']
+            review.rating = data['rating']
+            review.save()
+
         return Response({'created': True})
+    
+class UpdateReview(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, pk):
+        review = Review.objects.get(id=pk)
+        serializer = ReviewSerialzer(review, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response({'updated': True})
+        else:
+            return Response(serializer.errors)
+
+
+class DeleteReview(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def delete(self,request, pk):
+        data = None
+        try:
+            review = Review.objects.get(id=pk)
+            review.delete()
+            data = {'deleted': True}
+        except Review.DoesNotExist:
+           data = {'deleted': False}
+        
+        return Response(data)
+        
