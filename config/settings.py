8d6ecd90c5507b32396cdebd6288126d2b8d1a46
unknown <jonathannzete@gmail.com>
2022-02-18 20:56:47 +0100

    ALLOWED_HOST modified

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codegenesis-safekart.herokuapp.com', 'localhost']
 
 
 # Application definition
