9b384b881bc3445ef66600e1b94b8fc257d6b84a
unknown <jonathannzete@gmail.com>
2022-02-19 15:00:08 +0100

    database loaded

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath('static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
