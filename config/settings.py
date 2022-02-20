3a41b5c0bb29fad0aadbcfbc23c0ed12e7a5b358
unknown <jonathannzete@gmail.com>
2022-02-20 22:33:04 +0100

    CORS ALLOWED ORIGINS config updated

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # corsheaders config
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:3001",
     'https://codegenesis-safekart.netlify.app',
